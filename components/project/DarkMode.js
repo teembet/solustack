ee03a0ef18e73e57b2b9ed1a006bb6e45c1fcb05
Harbee-pearl <yinkaajibade97@gmail.com>
2022-10-16 12:30:32 +0100

    light and dark mode

--- components/project/DarkMode.js
+++ components/project/DarkMode.js
@@ -1,42 +1,57 @@
-import React from 'react'
-import DarkCard from './DarkCard'
+import React from "react";
+import DarkCard from "./DarkCard";
 
 const DarkMode = () => {
   return (
-    <div className="w-full overflow-x-hidden bg-[#1D2028]">
-    <div className="p-4 w-full lg:w-11/12 mx-auto py-16">
-      <div className="flex justify-between items-center">
-        <div className="font-poppins flex flex-col">
-          <h3 className="font-bold text-4xl mb-8">
-            <span className="text-purple2">Dark</span> Mode
-          </h3>
+    <>
+      <div className="w-full relative overflow-x-clip bg-[#1D2028] ">
+        <div className="p-4 w-full lg:w-11/12 mx-auto py-40 ">
+          <div className="flex justify-between items-center ">
+            <div className="font-poppins flex flex-col mr-6 xl:mr-0">
+              <h3 className="font-bold text-xl md:text-2xl xl:text-4xl mb-8 text-white">
+                <span className="text-purple2">
+                  <span className="font-light">|</span> Dark
+                </span>{" "}
+                Mode
+              </h3>
 
-          <div className="text-purple2 h-8 w-8 rounded-[50%] shadow-[0px_3px_38px_#00000029] border-3 border-white mb-8"></div>
-          <p className="font-medium text-xl text-white">
-            Design with a white <br /> background.
-          </p>
-        </div>
-        <div className="flex justify-between ">
-          <div className="mr-16 slant">
-            <DarkCard purple={true} shadowLg={true} />
+              <div className="bg-purple2 h-8 w-8 rounded-[50%] shadow-[0px_3px_38px_#00000029] border-3 border-white mb-8"></div>
+              <p className="font-medium text-sm lg:text-xl text-white">
+                Design with a white <br /> background.
+              </p>
+            </div>
+            <div className="">
+              <div className="flex justify-between slant ">
+                <div className=" mr-4 md:mr-8 xl:mr-16 ">
+                  <DarkCard purple={true} shadowLg={true} />
+                </div>
+                <div className="">
+                  <DarkCard shadowMd={true} />
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="slant">
-            <DarkCard  shadowMd={true} />
+        </div>
+
+        <div className="relative  h-[18rem] md:h-[28rem] xl:h-[42rem] flex justify-end">
+          <div className=" w-full xl:w-5/6  flex justify-end   slant">
+            <div className="hidden lg:block absolute bottom-56 left-6  xl:left-20 ">
+              <DarkCard shadowMd={true} />
+            </div>
+            <div className="w-3/5 flex justify-end mb-12   ">
+              <div className=" mr-6   ">
+                <DarkCard />
+              </div>
+              <div className=" mb-12">
+                <DarkCard />
+              </div>
+            </div>
           </div>
         </div>
       </div>
-      <div></div>
-    </div>
-    <div className="w-full  overflow-x-hidden overflow-y-visible relative">
-      <div className="p-4 w-full lg:w-11/12 mx-auto flex justify-end overflow-y-visible z-10">
-      
-      <div className="slant mr-16"><DarkCard /></div>
-      <div className="slant mr-16"><DarkCard /></div>
-      <div className="slant"><DarkCard /></div>
-      </div>
-    </div>
-  </div>
-  )
-}
+      <div className="bg-white h-[4rem] md:h-[8rem]  m-0 p-0"></div>
+    </>
+  );
+};
 
-export default DarkMode
+export default DarkMode;
--- components/project/LightMode.js
+++ components/project/LightMode.js
@@ -3,40 +3,51 @@ import WhiteCard from "./WhiteCard";
 
 const LightMode = () => {
   return (
-    <div className="w-full overflow-x-hidden">
-      <div className="p-4 w-full lg:w-11/12 mx-auto py-16">
-        <div className="flex justify-between items-center">
-          <div className="font-poppins flex flex-col">
-            <h3 className="font-bold text-4xl mb-8">
-              <span className="text-purple2">Light</span> Mode
-            </h3>
+    <>
+      <div className="w-full relative overflow-x-clip  overflow-y-visible ">
+        <div className="p-4 w-full lg:w-11/12 mx-auto py-40 ">
+          <div className="flex justify-between items-center ">
+            <div className="font-poppins flex flex-col mr-6 xl:mr-0">
+              <h3 className="font-bold text-xl md:text-2xl xl:text-4xl mb-8">
+                <span className="text-purple2">Light</span> Mode
+              </h3>
 
-            <div className="bg-purple2 h-8 w-8 rounded-[50%] shadow-[0px_3px_38px_#00000029] border-3 border-white mb-8"></div>
-            <p className="font-medium text-xl">
-              Design with a white <br /> background.
-            </p>
-          </div>
-          <div className="flex justify-between ">
-            <div className="mr-16 slant">
-              <WhiteCard  shadowLg={true} />
+              <div className="bg-purple2 h-8 w-8 rounded-[50%] shadow-[0px_3px_38px_#00000029] border-3 border-white mb-8"></div>
+              <p className="font-medium text-sm lg:text-xl">
+                Design with a white <br /> background.
+              </p>
             </div>
-            <div className="slant">
-              <WhiteCard  shadowMd={true} />
+            <div className="">
+              <div className="flex justify-between slant ">
+                <div className=" mr-4 md:mr-8 xl:mr-16 ">
+                  <WhiteCard border={true} shadowLg={true} />
+                </div>
+                <div className="">
+                  <WhiteCard shadowMd={true} />
+                </div>
+              </div>
             </div>
           </div>
         </div>
-        <div></div>
-      </div>
-      <div className="w-full bg-purple2 overflow-x-hidden overflow-y-visible relative">
-        <div className="p-4 w-full lg:w-11/12 mx-auto flex justify-end overflow-y-visible z-10">
 
-        
-        <div className="slant mr-16"><WhiteCard /></div>
-        <div className="slant mr-16"><WhiteCard /></div>
-        <div className="slant"><WhiteCard /></div>
+        <div className="relative bg-purple2 h-[18rem] md:h-[28rem] xl:h-[42rem] flex justify-end">
+          <div className=" w-full xl:w-5/6  flex justify-end   slant">
+            <div className="hidden lg:block absolute bottom-56 left-6  xl:left-20 ">
+              <WhiteCard shadowMd={true} />
+            </div>
+            <div className="w-3/5 flex justify-end mb-12   ">
+              <div className=" mr-6   ">
+                <WhiteCard />
+              </div>
+              <div className=" mb-12">
+                <WhiteCard />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+      <div className="bg-[#1D2028] h-[4rem] md:h-[10rem]  m-0 p-0"></div>
+    </>
   );
 };
 
