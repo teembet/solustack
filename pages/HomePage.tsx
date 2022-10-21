a8eec04ee3237ad58cbce71041d56e310fc5ad2f
Timbet Ekanem <Timbet.Ekanem@TimbetEkanem.local>
2022-10-21 16:41:21 +0100

    arrange

--- pages/HomePage.tsx
+++ pages/HomePage.tsx
@@ -10,29 +10,77 @@ import Image from "next/image";
 import * as React from "react";
 import Circle from "../public/icons/circle.svg";
 import Footer from "../components/shared/Footer";
-
+import Wrapper from "../element/Wrapper";
+import ScrollWrapper from "../element/ScrollWrapper";
+import { useInView } from "react-intersection-observer";
 const HomePage = ({ projects }) => {
-  return (
-    <div>
-      <Header />
-      <AboutSection />
-      <Services />
-      <Process />
-      {projects && <FeaturedProjects projects={projects} />}
-      <WhyUs />
+  const [ref, inView] = useInView({
+    threshold: 0.1,
+  });
 
-      <div className="relative overflow-x-clip">
-        <div
-          className="absolute bg-blue1 rounded-[50%] h-32 w-32 -right-20 -top-16 lg:h-40 lg:w-40 lg:-top-20 lg:-right-24   xl:h-52 xl:w-48 xl:-right-28 xl:-top-24"
-          style={{
-            backgroundImage: `url('${Circle.src}')`,
-          }}
-        ></div>
-        <Testimonial />
+  const [ref2, inView2] = useInView({
+    threshold: 0.1,
+  });
+
+  const activeTab = () => {
+    if (inView) {
+      return 1;
+    } else if (inView2 && !inView) {
+      return 2;
+    }
+  };
+
+  console.log(activeTab(), "active tab");
+  return (
+    <ScrollWrapper inView={inView}>
+      <div>
+        <Wrapper ref={ref}>
+          <section>
+            <Header />
+          </section>
+        </Wrapper>
+
+        <Wrapper ref={ref2}>
+          <section>
+            <AboutSection />
+          </section>
+        </Wrapper>
+
+        <Wrapper>
+          <section>
+            <Services />
+          </section>
+        </Wrapper>
+
+        <Wrapper>
+          <section>
+            <Process />
+          </section>
+        </Wrapper>
+
+        <Wrapper>
+          <section>
+            {projects && <FeaturedProjects projects={projects} />}
+          </section>
+        </Wrapper>
+        <Wrapper>
+          <section>
+            <WhyUs />
+          </section>
+        </Wrapper>
+        <div className="relative overflow-x-clip">
+          <div
+            className="absolute bg-blue1 rounded-[50%] h-32 w-32 -right-20 -top-16 lg:h-40 lg:w-40 lg:-top-20 lg:-right-24   xl:h-52 xl:w-48 xl:-right-28 xl:-top-24"
+            style={{
+              backgroundImage: `url('${Circle.src}')`,
+            }}
+          ></div>
+          <Testimonial />
+        </div>
+        <Contact />
+        <Footer />
       </div>
-      <Contact />
-      <Footer />
-    </div>
+    </ScrollWrapper>
   );
 };
 
